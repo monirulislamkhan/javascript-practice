ffffec914102263550a124012e3b5c093b1fc179
monirulislamkhan <mr.monirulkhan@gmail.com>
2023-12-21 15:32:08 +0530

    some feature added

--- console.js
+++ console.js
@@ -1,6 +1,6 @@
 "use strict";
 // console.log(NaN + 1);
-console.log(Boolean(1));
+/* console.log(Boolean(1));
 console.log(Boolean(0));
 console.log(Boolean("hello"));
 console.log(Boolean(""));
@@ -27,3 +27,54 @@ let d = 1;
 console.log(+d);
 console.log(+true);
 console.log(+"");
+
+console.log(2 > 1);
+console.log(2 == 1);
+console.log(2 != "2"); */
+
+// if(0) console.log('');
+
+// if(1||0) console.log('true')
+// let hour = 9;
+// if (hour < 10 || hour > 18) console.log("The office is closed.");
+// console.log(1 && 0);
+// console.log(1 && "Monirul");
+
+// console.log(false || "");
+
+// let firstName = "";
+// let lastName = "";
+// let nickName = "";
+
+// console.log(firstName || lastName || nickName || "Anonymous" || 2);
+
+// let hour = 12;
+// let minute = 10;
+// let result = hour === 1 || "The time is 12:30";
+// console.log(result);
+
+/* let x = 1;
+let res = x > 0 && "Greater than zero!";
+console.log(res); */
+
+/* console.log(!"");
+console.log(!0);
+console.log(!!"non-empty string");
+console.log(!null); */
+
+// alert(alert(1) || alert(3));
+// // alert(1 && null && 2);
+
+
+let userName = prompt(`Who's there?`, "");
+// let userName = "Admin";
+if (userName === "Admin") {
+  // if (userName) {
+  //   console.log("Enter Your Password");
+  // }
+  alert("Welcome");
+} else if (userName === !"Admin") {
+  alert("I don't know you");
+} else {
+  alert("Canceled");
+}
